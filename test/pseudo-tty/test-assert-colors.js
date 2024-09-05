6776ea356a0d7a823188c52965ad0541895f6558
Giovanni <github@puskin.it>
2024-09-05 16:43:45 +0200

    assert: reintroduced blue color and fixed colors test

--- test/pseudo-tty/test-assert-colors.js
+++ test/pseudo-tty/test-assert-colors.js
@@ -9,9 +9,12 @@ assert.throws(() => {
   assert.deepStrictEqual([1, 2, 2, 2, 2], [2, 2, 2, 2, 2]);
 }, (err) => {
   const expected = 'Expected values to be strictly deep-equal:\n' +
-    '\u001b[32m+ actual\u001b[39m \u001b[31m- expected\u001b[39m' +
-      ' \u001b[34m...\u001b[39m Lines skipped\n\n' +
-    '  [\n' +
+    '\x1B[32m+ actual\x1B[39m \x1B[31m- expected\x1B[39m\n' +
+    "\x1B[34m...\x1B[39m Lines skipped which didn't differ\n" +
+    '\x1B[32m...\x1B[39m Lines skipped which were identical and inserted\n' +
+    '\x1B[31m...\x1B[39m Lines skipped which were identical and deleted\n' +
+    '\n' +
+    '\x1B[39m  [\n' +
     '\u001b[32m+\u001b[39m   1,\n' +
     '\u001b[31m-\u001b[39m   2,\n' +
     '    2,\n' +
